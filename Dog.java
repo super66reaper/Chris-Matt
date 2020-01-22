8030ce7e55c7590b77c7080ba69ca7af04234282
Chris Urbano <ChrisUrbano1116@gmail.com>
2020-01-22 08:25:06 -0700

    made dog class

---
+++ Dog.java
@@ -0,0 +1,63 @@
+
+public class Dog
+{
+    private String name;
+    private boolean fed;
+    private boolean walked;
+    private boolean petted;
+    private boolean happy;
+
+    public dog(String name){
+        this.name = name;
+    }
+
+    public bool getFed()
+    {
+        return fed;
+    }
+    public bool getWalked()
+    {
+        return walked;    
+    }
+    public bool getPetted()
+    {
+        return petted;
+    }
+
+    
+    public void feed(boolean condition)
+    {
+        this.fed = condition
+
+    }
+    public void walk(boolean condition)
+    {
+        this.walked = condition;
+
+    }
+    public void pet(boolean condition)
+    {
+        this.petted = condition;
+
+    }
+
+    public boolean getHappy()
+    {
+        return happy;
+    }
+
+    public String getMood()
+    {
+        if(fed && walked && petted){
+            happy = true;
+            return "WOOF!";
+            
+        }
+        else{
+            happy = false;
+            return "woof";
+        }
+    }
+    
+
+}
